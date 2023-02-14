eca1cf4e1e0624ca679b9e48feaf8d524a1f820a
Chen Siyuan <s1yuan_chen@163.com>
2023-02-14 18:39:56 +0800

    feat: ioc complete.

---
+++ src/main/java/csy/example/App2.java
@@ -0,0 +1,26 @@
+package csy.example;
+
+import csy.example.dao.BookDao;
+import csy.example.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//step3 创建新Application
+public class App2 {
+
+    public static void main(String[] args) {
+        //step4 使用IoC容器获取并调用目标bean的方法
+        //获取IoC容器
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //获取DAO的bean
+        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+        //调用
+        bookDao.save();
+        System.out.println("=========================");
+        //获取Service的bean
+        BookService bookService = (BookService) ctx.getBean("bookService");
+        //调用
+        bookService.save();
+
+    }
+}
